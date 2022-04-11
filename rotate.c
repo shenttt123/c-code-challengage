764766671bc1dd98c3bb8d3d4ac470e150d092d4
shenttt123 <98625344+shenttt123@users.noreply.github.com>
2022-04-11 15:42:52 -0400

    Add files via upload

---
+++ rotate.c
@@ -0,0 +1,47 @@
+//https://www.linkedin.com/learning/c-code-challenges/rotating-a-matrix?autoplay=true&resume=false
+
+#include <stdio.h>
+
+struct matrix{
+
+char a[10][10];
+
+};
+int main(){
+
+srand( (unsigned)time(NULL) );
+
+populatematrix(3,8,1);
+populatematrix(5,5,2);
+populatematrix(6,4,3);
+
+return(0);
+}
+
+void populatematrix(int row, int col, int count){
+
+int x,y;
+struct matrix mar;
+struct matrix temp;
+
+int random;
+printf("Original Matrix: %d\n", count);
+for (x=0;x<row;x++){//generate matrix
+    for (y=0;y<col;y++){
+            mar.a[x][y]=rand()%26+ 'a';
+            temp.a[x][y]=mar.a[x][y];//copy into temp
+            printf("%c ",mar.a[x][y]);
+        } printf("\n");
+}
+printf("Rotated Matrix: %d\n", count);
+
+
+for (x=0;x<col;x++){//rotate
+    for (y=0;y<row;y++){
+        mar.a[x][y]=temp.a[row-y-1][x];
+        printf("%c ",mar.a[x][y]);
+    }printf("\n");
+}
+
+}
+
